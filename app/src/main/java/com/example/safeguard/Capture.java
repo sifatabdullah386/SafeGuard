178ad7f066fe2ea44eaeee8ce83f1585ce0e7e17
Smile_Sifat <smilesifat@gmail.com>
2020-01-14 17:19:22 +0600

    Commit15

--- app/src/main/java/com/example/safeguard/Capture.java
+++ app/src/main/java/com/example/safeguard/Capture.java
@@ -1,9 +1,13 @@
 package  com.example.safeguard;
 
+import android.Manifest;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,61 +18,100 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 import static android.app.Activity.RESULT_OK;
+import static android.provider.MediaStore.ACTION_IMAGE_CAPTURE;
 
 public class Capture extends Fragment {
 
-    private ImageView CaptureImage;
-    private Button Capture;
-    private static final int CAMERA_REQUEST_CODE=1;
-    private StorageReference picStorage;
+    private ImageView captureImage;
+    private static final int CAMERA_REQUEST_CODE=71;
+    private static final int CAMERA_PERM_CODE=7;
+    private StorageReference storageReference;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_capture, container, false);
 
-        picStorage= FirebaseStorage.getInstance().getReference();
+        storageReference = FirebaseStorage.getInstance().getReference();
 
-        CaptureImage = view.findViewById(R.id.image_capture);
-        Capture = view.findViewById(R.id.image_capture_button);
+        captureImage = view.findViewById(R.id.image_capture);
+        Button Capture = view.findViewById(R.id.image_capture_button);
+        Button Upload = view.findViewById(R.id.image_upload_button);
+
         Capture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,CAMERA_REQUEST_CODE);
+                askCameraPermissions();
+            }
+        });
+        Upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UploadImage();
             }
         });
         return view;
+    }
+
+    private void askCameraPermissions() {
+        if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(getActivity(), new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        }else{
+            openCamera();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==CAMERA_PERM_CODE){
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+              openCamera();
+            }
+            else{
+                Toast.makeText(getActivity(), "Camera open Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openCamera() {
+        Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,CAMERA_REQUEST_CODE);
     }
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-/*        if(requestCode==CAMERA_REQUEST_CODE && resultCode==RESULT_OK){
-            assert data != null;
-            Uri uri=data.getData();
-            assert uri != null;
-            StorageReference filepath=picStorage.child("Captures").child(Objects.requireNonNull(uri.getLastPathSegment()));
-            filepath.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Toast.makeText(getActivity(), "", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }*/
-        assert data != null;
-        Bitmap bitmap=(Bitmap) data.getExtras().get("data");
-        CaptureImage.setImageBitmap(bitmap);
+      if(requestCode==CAMERA_REQUEST_CODE && resultCode==RESULT_OK ){
+
+        /*assert data != null;
+        Bitmap bitmap=(Bitmap) Objects.requireNonNull(data.getExtras()).get("data");
+        captureImage.setImageBitmap(bitmap);*/
+
+      }
+    }
+
+
+    private void UploadImage() {
 
     }
 }
