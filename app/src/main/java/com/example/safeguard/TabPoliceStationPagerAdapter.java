e5e0a07fc475c7044c53b8d8c72d9648c8321e2c
Smile_Sifat <smilesifat@gmail.com>
2020-01-16 00:48:54 +0600

    Commit16

---
+++ app/src/main/java/com/example/safeguard/TabPoliceStationPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.safeguard;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class TabPoliceStationPagerAdapter extends FragmentStatePagerAdapter {
+    private int tabCount;
+
+    TabPoliceStationPagerAdapter(FragmentManager fm, int tabCount) {
+        super(fm);
+        this.tabCount= tabCount;
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                TabPoliceStationList tabPoliceStationList = new TabPoliceStationList();
+                return tabPoliceStationList;
+            case 1:
+                TabPoliceStationMaps tabPoliceStationMaps = new TabPoliceStationMaps();
+                return tabPoliceStationMaps;
+            default:
+                return null;
+        }
+    }
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
